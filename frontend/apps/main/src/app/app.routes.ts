79d786e2ce79b775beec897880c3f7f70d21b864
Yuki61803 <Yuki61803@gmail.com>
2025-01-15 13:16:27 +0800

    feat(frontend): add nx workspace

---
+++ frontend/apps/main/src/app/app.routes.ts
@@ -0,0 +1,13 @@
+import { NxWelcomeComponent } from './nx-welcome.component';
+import { Route } from '@angular/router';
+
+export const appRoutes: Route[] = [
+  {
+    path: 'profile',
+    loadChildren: () => import('profile/Routes').then((m) => m!.remoteRoutes),
+  },
+  {
+    path: '',
+    component: NxWelcomeComponent,
+  },
+];
